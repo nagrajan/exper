e1472e850acab91b4c5d1438afa494202727dea5
Nagarajan <nag.rajan@gmail.com>
2013-06-27 11:16:46 -0700

    django settings and gitignore changes

--- django/cmstest/cmstest/settings.py
+++ django/cmstest/cmstest/settings.py
@@ -158,9 +158,9 @@ INSTALLED_APPS = (
     'cms.plugins.video',
     'cms.plugins.twitter',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
